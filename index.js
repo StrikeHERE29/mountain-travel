d41087e209a99cf9c620ce2198a892035fc99361
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-06-04 08:46:10 +0300

    finished the couple-hiking section

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let items = document.querySelectorAll('.slider .item');
         items[active].style.zIndex = 1;
         items[active].style.filter = 'none';
         items[active].style.opacity = 1;
-        for(var i = active + 1; i < items.length; i++){
+        for(let i = active + 1; i < items.length; i++){
             stt++;
             items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
             items[i].style.zIndex = -stt;
@@ -17,7 +17,7 @@ let items = document.querySelectorAll('.slider .item');
             items[i].style.opacity = stt > 2 ? 0 : 0.6;
         }
         stt = 0;
-        for(var i = active - 1; i >= 0; i--){
+        for(let i = active - 1; i >= 0; i--){
             stt++;
             items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
             items[i].style.zIndex = -stt;
